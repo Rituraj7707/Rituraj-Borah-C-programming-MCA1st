996669fe817bc1a3e3210341b288cc7d1e920b27
Rituraj7707 <riturjbora023@gmail.com>
2024-09-27 12:17:00 +0530

    Add files via upload

---
+++ stringcpy.c
@@ -0,0 +1,14 @@
+//prog name: sting copy
+//Rituraj Borah
+//prog date: 27-09-2024
+
+#include<stdio.h>
+#include<string.h>
+main()
+{
+	char fname[20],cname[20];
+	printf("\nEnter first name:");
+	scanf("%s",fname);
+	strcpy(cname,fname);
+	printf("\nCopied Name:%s",cname);
+}
